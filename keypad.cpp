086e69e4ce520a7aa925b530a8ce88ab23c46443
Hasatee <143634039+Hasatee@users.noreply.github.com>
2025-03-13 23:35:44 +0700

    Add files via upload

---
+++ keypad.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <Keypad.h>
+
+#define ROW_NUM     4 // four rows
+#define COLUMN_NUM  4 // four columns
+
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+//byte pin_rows[ROW_NUM]      = {19, 18, 5, 17}; // GPIO19, GPIO18, GPIO5, GPIO17 connect to the row pins
+//byte pin_column[COLUMN_NUM] = {15, 2, 4, 16};   // GPIO16, GPIO4, GPIO2, GPIO15 connect to the column pins
+
+byte pin_rows[ROW_NUM]      = {19, 18, 5, 17}; // GPIO19, GPIO18, GPIO5, GPIO17 connect to the row pins
+byte pin_column[COLUMN_NUM] = {16, 4, 2, 15};   // GPIO16, GPIO4, GPIO2, GPIO15 connect to the column pins
+
+Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char key = keypad.getKey();
+
+  if (key) {
+    Serial.println(key);
+  }
+}
