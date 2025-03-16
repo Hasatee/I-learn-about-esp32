5e6af38b23eb4aa67a96e5f6768b6a0537592625
Hasatee <143634039+Hasatee@users.noreply.github.com>
2025-03-16 21:30:20 +0700

    Update lcd.cpp

--- lcd.cpp
+++ lcd.cpp
@@ -13,13 +13,13 @@ void loop() {
   lcd_i2c.setCursor(0, 0);      // move cursor to   (0, 0)
   lcd_i2c.print("Hello");       // print message at (0, 0)
   lcd_i2c.setCursor(2, 1);      // move cursor to   (2, 1)
-  lcd_i2c.print("esp32io.com"); // print message at (2, 1)
+  lcd_i2c.print("Hasatee"); // print message at (2, 1)
   delay(2000);                  // display the above for two seconds
 
   lcd_i2c.clear();                  // clear display
   lcd_i2c.setCursor(3, 0);          // move cursor to   (3, 0)
-  lcd_i2c.print("DIYables");        // print message at (3, 0)
+  lcd_i2c.print("LCD");        // print message at (3, 0)
   lcd_i2c.setCursor(0, 1);          // move cursor to   (0, 1)
-  lcd_i2c.print("www.diyables.io"); // print message at (0, 1)
+  lcd_i2c.print("Monitor"); // print message at (0, 1)
   delay(2000);                      // display the above for two seconds
 }
